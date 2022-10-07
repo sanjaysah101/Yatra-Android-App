2d89debddea237fb73d800aa93575789959f15db
Nayan343 <nshah343@rku.ac.in>
2022-10-08 00:05:09 +0530

    profile implementation

--- app/src/main/java/com/example/yatra/Fragments/AccountFragment.java
+++ app/src/main/java/com/example/yatra/Fragments/AccountFragment.java
@@ -1,15 +1,32 @@
 package com.example.yatra.Fragments;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.yatra.MainActivity;
 import com.example.yatra.R;
+import com.example.yatra.SignInActivity;
+import com.example.yatra.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AccountFragment extends Fragment {
 
@@ -23,10 +40,56 @@ public class AccountFragment extends Fragment {
         super.onCreate(savedInstanceState);
         View view =  inflater.inflate(R.layout.fragment_home, container, false);
         Button logoutButton = view.findViewById(R.id.logoutButton);
+        TextView uName = view.findViewById(R.id.uName);
+        TextView userEmail = view.findViewById(R.id.userEmail);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setMessage("Do you want to logout");
+                builder.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        logoutUser();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+            }
+        });
 
+
+//        ############################### if user is not log in logout send to signInActivity  ########################
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            startActivity(new Intent(getContext(), SignInActivity.class));
+            (getActivity()).finish();
+        }
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference("Users").child(currentUser.getUid());
+
+        //        ########################### Fetching User's Detail from Firebase ################################
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+
+                User user = snapshot.getValue(User.class);
+                if(user != null){
+                    uName.setText(user.getFullName());
+                    userEmail.setText(user.getEmail());
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -39,5 +102,12 @@ public class AccountFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+    private void logoutUser(){
+//      ##################   Logout Function #######################3
+        FirebaseAuth.getInstance().signOut();
+        Intent signInIntent = new Intent(getContext(), SignInActivity.class);
+        startActivity(signInIntent);
+        (getActivity()).finish();
     }
 }
--- app/src/main/java/com/example/yatra/User.java
+++ app/src/main/java/com/example/yatra/User.java
@@ -3,9 +3,9 @@ package com.example.yatra;
 public class User {
     public String fname, lname, email;
     public String fullName;
-    public User(){
-        
-    }
+
+    public User(){}
+
     public User(String fullname,String email){
         this.fullName = fullname;
         this.email = email;
@@ -14,5 +14,37 @@ public class User {
         this.fname = fname;
         this.lname = lname;
         this.email = email;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 }
